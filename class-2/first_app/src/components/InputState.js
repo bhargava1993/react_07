c7979f73d2507100cf2041af45f2ffdc350b314a
unknown <1993bhargava@gmail.com>
2026-09-11 08:29:15 +0530

    updated code

--- class-2/first_app/src/components/InputState.js
+++ class-2/first_app/src/components/InputState.js
@@ -1,71 +1,62 @@
 import { useState } from "react";
 
 function InputState() {
-    const [name, setName] = useState("");
-    const [age, setAge] = useState("");
-    const [salary, setSalary] = useState("");
 
-    const [empValue, setEmpValue] = useState({
-        name: "",
-        age: "",
-        salary: ""
-    });
+    const [name, SetName] = useState("");
+    const [age, SetAge] = useState("");
+    const [salary, SetSalary] = useState("");
 
-    const [groupEmp, setGroupEmp] = useState([]);
+    const sampleObj = { "name": "", "age": "", "salary": "" };
+    const [empValue, SetEmpValue] = useState(sampleObj);
+
+    const [groupEmp, SetGroupEmp] = useState([]);
+
+    function handleInput(event) {
+        // console.log(event.target.value)
+        SetName(event.target.value)
+        // SetEmpValue({...empValue,name});
+        // console.log("empValue---", empValue)
+    }
 
     function handleSubmit() {
-        const newEmployee = {
-            name,
-            age,
-            salary
-        };
+        // SetEmpValue({name,age,salary});
+        // console.log("empValue---", empValue)
+        // SetGroupEmp([...groupEmp,empValue]);
 
-        setEmpValue(newEmployee);
+        let newEmp = {name,age,salary};
+        SetEmpValue(newEmp)
 
-        setGroupEmp((prev) => [...prev, newEmployee]);
-
-        console.log("newEmployee---", newEmployee);
+        SetGroupEmp((prev)=>[...prev,newEmp]);
+        console.log("groupEmp---", groupEmp)
 
         handleClear();
     }
 
-    function handleClear() {
-        setName("");
-        setAge("");
-        setSalary("");
+    function handleClear(){
+            SetName("");
+            SetAge("");
+            SetSalary("");
     }
-
     return (
         <>
             <label>Name:</label>
-            <input
-                type="text"
-                placeholder="Enter name"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-            />
-
-            <br />
-
+            <input type="text" placeholder="Enter name"
+                onChange={handleInput} id="nameID" value={name}/>
+            <br></br>
             <label>Age:</label>
-            <input
-                onChange={(e) => setAge(e.target.value)}
-                value={age}
-            />
 
-            <br />
+            <input onChange={(e) => SetAge(e.target.value)} value={age} />
 
+            <br></br>
             <label>Salary:</label>
-            <input
-                onChange={(e) => setSalary(e.target.value)}
-                value={salary}
-            />
+
+            <input onChange={(e) => SetSalary(e.target.value)} value={salary}/>
 
             <h1>Hello: {name}</h1>
-            <h1>Age: {age}</h1>
+            <h1>Age:{age}</h1>
             <h1>Salary: {salary}</h1>
 
-            <button onClick={handleSubmit}>Submit</button>
+            <button onClick={handleSubmit}>submit</button>
             <button onClick={handleClear}>Clear</button>
 
             <h2>Employees</h2>
